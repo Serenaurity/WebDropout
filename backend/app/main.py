a56260d7f4e0c36026c441c6030806755d19feba
Serenaurity <serendip.au@gmail.com>
2025-12-09 22:40:23 +0700

    web edit

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://dropout-teacher-portal.onrender.com",
+        "https://dropout-frontend.onrender.com"
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
